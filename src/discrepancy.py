69ccd8e5f0b710c1958f7b0a7ff717afecc2904b
amastromartino <a.mastromartino@ce.pucmm.edu.do>
2025-12-02 13:46:28 -0400

    Delete src directory

--- src/discrepancy.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def compute_discrepancy(system1, system2):
-    """
-    Computes discrepancy between a system and its involuted image.
-    This is used as the integrability criterion in the article.
-    """
-    return sum(abs(system1[key] - system2[key]) for key in system1)
--- src/prolongation.py
+++
@@ -1,9 +0,0 @@
-def prolong(system, order=1):
-    """
-    Computes the k-th prolongation of a differential system.
-    This is a simplified version used for benchmark structure.
-    """
-
-    prolonged = system.copy()
-    # In a real setting you would add partial derivatives of higher order
-    return prolonged
