e57c0478b1af3adfacfbb553b284bc339504bd9d
amastromartino <a.mastromartino@ce.pucmm.edu.do>
2025-12-03 10:09:19 -0400

    Update involutive_example.py

--- examples/involutive_example.py
+++ examples/involutive_example.py
@@ -1,4 +1,9 @@
-from src.main import check_involution_condition
+# involutive_example.py
+import os, sys
+# Añadir automáticamente la ruta del proyecto (sube desde /src/examples/ a /src/)
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from main import check_involution_condition
 import sympy as sp
 
 # Ejemplo involutivo: (y')^2 + y^2 - 1 = 0
@@ -7,6 +12,9 @@ x = sp.Symbol("x")
 y = sp.Function("y")(x)
 p = sp.diff(y, x)
 
+# Representación del sistema:
+#   u_x = p       (derivada)
+#   u   = y       (estado)
 system = {
     "u_x": p,
     "u": y
@@ -15,3 +23,4 @@ system = {
 print("Checking involution of: (y')^2 + y^2 - 1 = 0")
 result = check_involution_condition(system, order_k=1)
 print(result)
+
