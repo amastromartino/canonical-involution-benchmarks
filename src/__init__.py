caead09b64537b0478ff04d434c89af29f2c6ad2
amastromartino <a.mastromartino@ce.pucmm.edu.do>
2025-12-03 12:31:43 -0400

    Update __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+Canonical Involution Benchmarks Package
+"""
+
+from .main import check_involution_condition
+
+__version__ = "0.1.0"
+__author__ = "Ángel Mastromartino"
+__all__ = ["check_involution_condition"]
